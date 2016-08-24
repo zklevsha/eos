89efe0577a97441cb7f550bf5fa44759ff8cd023
kzhukov <zklevsha@gmail.com>
2016-08-24 18:45:10 +0300

    finished generate_csv

--- csv_generate.py
+++ csv_generate.py
@@ -1,19 +1,23 @@
 import csv
 import pickle
 from  mylibs.utils import get_logger
+import sys
+
 data = pickle.load( open('data.p','rb') )
 
 log = get_logger('csv_generate.txt')
 
 with open('csv_out.csv', 'w') as csvfile:
-	cw = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-	header = ['End-of-Sale Date',
+	cw = csv.writer(csvfile, delimiter=',')
+	header = ['PN',
+				'End-of-Sale Date',
 				 'End of New Service Attachment Date',
 				 'End of New Service Attachment Date App'
 				 'End of Service Contract Renewal Date',
 				 'End of Service Contract Renewal Date App',
 				 'Last Date of Support',
-				 'Last Date of Support App']
+				 'Last Date of Support App',
+				 'Source Link']
 
 	cw.writerow(header)
 
@@ -36,28 +40,31 @@ with open('csv_out.csv', 'w') as csvfile:
 		nonMandatory = [second,secondApp,third,thirdApp,forth,forthApp]
 
 		for k in dates.keys():
-			if 'End-of-Sale' in k:
+			search_k = k.lower().replace('-','').replace(' ','').replace('\n','')
+			print (search_k)
+
+			if 'endofsale' in search_k:
 				first.append(dates[k])
 
-			if 'Attachment' in k:
-				if 'App' in k:
+			if 'attachment' in search_k:
+				if 'app' in search_k:
 					secondApp.append(dates[k])
 				else:
 					second.append(dates[k])
 
-			if 'Renewal' in k and 'App' not in k:
-				if 'App' in 'k':
+			if 'renewal' in search_k:
+				if 'app' in search_k:
 					thirdApp.append(dates[k])
 				else:
 					third.append(dates[k])
 
-			if 'Support' in k and 'Phone' not in k:
-				if 'App' in k:
+			if 'lastdateofsupport' in search_k and 'phone' not in search_k:
+				if 'app' in search_k:
 					forthApp.append(dates[k])
 				else:
 					forth.append(dates[k])
 
-	
+		
 
 		if len(first) != 1 or any( len(i) > 1 for i in nonMandatory ):
 			log.error('Cant parse ' + data_k)
@@ -71,13 +78,9 @@ with open('csv_out.csv', 'w') as csvfile:
 				log.info(repr(i))
 
 			log.info('url:' + data[data_k]['doc'][1])
-
+			log.info('')
+			log.info(data[data_k])
 			sys.exit()
 
-		else:
-			
-			for i in nonMandatory:
-				if len(i) == 0:
-					i = ['N/A']
-
-			cw.writerow( [ i[0] for i in [first + nonMandatory] ] )
+		line =  [ [data_k],first,second,secondApp,third,thirdApp,forth,forthApp, [data[data_k]['doc'][1] ] ]  
+		print(line)
